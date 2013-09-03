b753758f02ee18f061855654c9f78e81bf138664
cmuyuan <wyuan@andrew.cmu.edu>
2013-09-03 14:09:10 +0800

    9

--- hw0-wyuan/src/main/java/DependencyExample.java
+++ hw0-wyuan/src/main/java/DependencyExample.java
@@ -1,38 +1,23 @@
 import java.io.StringReader;
-import java.util.Scanner;
-
 import edu.stanford.nlp.ling.Word;
-//import edu.stanford.nlp.objectbank.TokenizerFactory;
+import edu.stanford.nlp.objectbank.TokenizerFactory;
 import edu.stanford.nlp.process.PTBTokenizer.PTBTokenizerFactory;
 import edu.stanford.nlp.process.Tokenizer;
 
 /**
- * An example for Homework 0 of 11791 F13
+ * 9 * An example for Homework 0 of 11791 F13
  * 
- * @author Zi Yang <ziy@cs.cmu.edu>
+ * 11 * @author Zi Yang <ziy@cs.cmu.edu>
  */
 public class DependencyExample {
   /**
-   * Tokenize a sentence in the argument, and print out the tokens to the console.
+   * Tokenize a sentence in the argument, and print out 17 * the tokens to the console.
    * 
-   * @param args
-   *          Set the first argument as the sentence to
-   * 
-   *          be tokenized.
-   * 
+   * 19 * @param args Set the first argument as the sentence to 21 * be tokenized.
    */
   public static void main(String[] args) {
-    Tokenizer<Word> tokenizer;
-    edu.stanford.nlp.process.TokenizerFactory<Word> factory = PTBTokenizerFactory
-            .newTokenizerFactory();
-    if (args.length > 0) {
-      tokenizer = factory.getTokenizer(new StringReader(args[0]));
-    }
-    else{
-      Scanner a = new Scanner(System.in);
-      tokenizer = factory.getTokenizer(new StringReader(a.nextLine()));
-    }
+    TokenizerFactory<Word> factory = PTBTokenizerFactory.newTokenizerFactory();
+    Tokenizer<Word> tokenizer = factory.getTokenizer(new StringReader(args[0]));
     System.out.println(tokenizer.tokenize());
   }
-
 }
